9a9696b9232b5f1da148a8e6627d990dcd5840d2
Yachim <jachym.kohout@gmail.com>
2024-03-28 13:49:06 +0100

    gravitational lensing wip

--- src/app/general-relativity/schwarzschild/gravitational-lensing/page.tsx
+++ src/app/general-relativity/schwarzschild/gravitational-lensing/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Metadata } from "next";
+import { BlockMath, InlineMath } from "react-katex";
 
 export const metadata: Metadata = {
   title: "Gravitational Lensing"
@@ -10,6 +11,11 @@ export default async function Home() {
     <div className="article">
         <Link href="/general-relativity/schwarzschild">Back</Link>
         <h1>Gravitational Lensing</h1>
+        <p>I will reuse equation for energy from the <Link href="/general-relativity/schwarzschild/geodesics#relativistic-orbits">geodesics</Link> chapter:</p>
+        <BlockMath math="\mathcal{E}^2 = \left(\frac{dr}{d\lambda}\right)^2 + \frac{\mathcal{L}^2}{r^2} - \frac{2\mathcal{L}^2}{r^3} + \frac{2}{r} \epsilon - \epsilon." />
+
+        <p>We are considering the path of light rays (<InlineMath math="\epsilon = 0" />)</p>
+        <BlockMath math="\mathcal{E}^2 = \left(\frac{dr}{d\lambda}\right)^2 + \frac{\mathcal{L}^2}{r^2} - \frac{2\mathcal{L}^2}{r^3}." />
     </div>
   )
 }
